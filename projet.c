5f51e9c17f8a3b7947ec157d7d7a061ad42c4398
TouilRaed <147189010+TouilRaed@users.noreply.github.com>
2023-10-16 00:45:07 +0100

    Update projet.c

--- projet.c
+++ projet.c
@@ -1,196 +1,185 @@
 #include <stdio.h>
-#include<stdlib.h>
+#include <stdlib.h>
 
 typedef struct {
-  int ref;
-  int tailleMemoir;
-  int tailleDisque;
-  char sysExp[20];
-  char marque[20];
-}ordinateur;
+    int ref;
+    int tailleMemoir;
+    int tailleDisque;
+    char sysExp[20];
+    char marque[20];
+} ordinateur;
 
-typedef struct 
-{
-  int ref;
-  char marque[20];
-  char typeMoteur[20];
-  int vitesseMax;
-  int prix;
-}voiture;
-void ajouterOrd(ordinateur[], int * );
+typedef struct {
+    int ref;
+    char marque[20];
+    char typeMoteur[20];
+    int vitesseMax;
+    int prix;
+} voiture;
+
+void ajouterOrd(ordinateur[], int *);
 void ajouterVoit(voiture[], int *);
-void afficherVoit(voiture[],int,int *);
-void afficherOrd(ordinateur[],int, int *);
+void afficherVoit(voiture[], int, int);
+void afficherOrd(ordinateur[], int, int);
 
-static int i;
 int main(){
-  int choixRefProduit;
-  int nbVoit=0;
-  int nbOrd=0;
-  int choixAjouter,choixSupprimer,choixAfficher;
-  int choix;
-  int *PnbVoit=&nbVoit;
-  int *PnbOrd=&nbOrd;
-  ordinateur stockOrd[50];
-  printf("la capacité maximale de votre stockage d'ordinateur est de 50 \n");
-  voiture stockVoit[50];
-  printf("la capacité maximale de votre stockage de voiture est de 50 \n");
-  do
-  {                                              /*affichage du menu principale*/
-    printf("##### Menu Principal ##### \n");
-    printf("1.ajouter un produit \n");
-    printf("2.supprimer un produit \n");
-    printf("3.afficher les caracteristique d'un produit \n");
-    printf("pour arrêter le programme tapez 0 \n");
+    int choixRefProduit;
+    int nbVoit = 0;
+    int nbOrd = 0;
+    int choix;
+    ordinateur stockOrd[50];
+    printf("La capacité maximale de votre stockage d'ordinateur est de 50\n");
+    voiture stockVoit[50];
+    printf("La capacité maximale de votre stockage de voiture est de 50\n");
 
-    do
-  {                                             /*saisir le choix*/
-    printf("donner votre choix :");
-    scanf("%d \n",&choix);
-    if (choix<1 || choix>4)
-    {
-      printf("votre choix est invalide \n");
-      break;
-    }
-    
-    switch (choix)
-    {
-    case 1:                                          /* chois ajouter*/
-      printf("1.ajouter une voiture \n");
-      printf("2.ajouter un ordinateur \n");
-      scanf("donner votre choix %d \n",&choixAjouter);
-      switch (choixAjouter)
-      {
-      case 1:                                      /*ajouter une voiture*/
-        ajouterVoit(stockVoit,PnbVoit);
-        break;
-      case 2:                                     /*ajouter un ordinateur*/
-         ajouterOrd(stockOrd,PnbOrd);      
-         break;
-      default:
-        printf("choix invalide \n");
-        break;
-      }
-      break;
-      case 2:                                            /*choix de supprimer*/
-         printf("1.supprimer une voiture \n");
-         printf("2.supprimer un ordinateur \n");
-         scanf("donner votre choix %d \n",&choixSupprimer);
-       switch (choixSupprimer)
-       {
-       case 1:                                        /*supprimer une voiture*/
-        supprimerVoit(stockVoit,nbVoit,PnbVoit);
-        break;
-       case 2:                                       /*supprimer un ordinateur*/
-        supprimerOrd(stockOrd,nbOrd,PnbOrd);
-        break;
-       default:
-        printf("choix invalide \n");
-        break;
-       }
-       break;
-      case 3:                                     /*choix d'affichage*/
-       printf("1.afficher une voiture \n");
-       printf("2.afficher un ordinateur \n");
-       scanf("donner votre choix %d \n",&choixAfficher);
-       switch (choixAfficher)
-       {
-       case 1:                                   /*afficher une voiture*/
-        scanf("%d",&choixRefProduit);
-        afficherVoit(stockVoit,choixRefProduit,PnbVoit);
-        break;
-       case 2:                                   /*afficher un ordinateur*/
-        afficherOrd(stockOrd,choixRefProduit,PnbOrd);
-       
-       default:
-        printf("choix invalide \n");
-        break;
-       }
-      break;
-    
-    default:
-       printf("choix invalide \n");
-      break;
-    }
-  } while (choix<1 || choix >4);
-  
-  } while (choix!=0 && nbOrd<50 && nbVoit<50);
-  
-  
+    do {
+        /* Display the main menu */
+        printf("##### Menu Principal #####\n");
+        printf("1. Ajouter un produit\n");
+        printf("2. Supprimer un produit\n");
+        printf("3. Afficher les caractéristiques d'un produit\n");
+        printf("Pour arrêter le programme, tapez 0\n");
 
- return 0;
+        /* Get the user's choice */
+        printf("Donnez votre choix: ");
+        scanf("%d", &choix);
+
+        switch (choix) {
+            case 1: /* Choice to add */
+                printf("1. Ajouter une voiture\n");
+                printf("2. Ajouter un ordinateur\n");
+                int choixAjouter;
+                scanf("%d", &choixAjouter);
+
+                switch (choixAjouter) {
+                    case 1:
+                        ajouterVoit(stockVoit, &nbVoit);
+                        break;
+                    case 2:
+                        ajouterOrd(stockOrd, &nbOrd);
+                        break;
+                    default:
+                        printf("Choix invalide\n");
+                        break;
+                }
+                break;
+
+            case 2: /* Choice to delete - You need to implement this function */
+                printf("1. Supprimer une voiture\n");
+                printf("2. Supprimer un ordinateur\n");
+                int choixSupprimer;
+                scanf("%d", &choixSupprimer);
+
+                switch (choixSupprimer) {
+                    // Implement the deletion logic
+                }
+                break;
+
+            case 3: /* Choice to display */
+                printf("1. Afficher une voiture\n");
+                printf("2. Afficher un ordinateur\n");
+                int choixAfficher;
+                scanf("%d", &choixAfficher);
+
+                switch (choixAfficher) {
+                    case 1:
+                        scanf("%d", &choixRefProduit);
+                        afficherVoit(stockVoit, choixRefProduit, nbVoit);
+                        break;
+                    case 2:
+                        scanf("%d", &choixRefProduit);
+                        afficherOrd(stockOrd, choixRefProduit, nbOrd);
+                        break;
+                    default:
+                        printf("Choix invalide\n");
+                        break;
+                }
+                break;
+
+            case 0: /* Exit the program */
+                break;
+
+            default:
+                printf("Choix invalide\n");
+                break;
+        }
+    } while (choix != 0);
+
+    return 0;
 }
 
-
-void ajouterOrd(ordinateur stockOrd[], int *PnbOrd ){
-  if (*PnbOrd==50)
-  {
-    printf("stock est plein  \n");
-  }
-  else{
-  printf("donner reference de l'ordinateur \n");
-  scanf("%d",&stockOrd[*PnbOrd].ref);
-  printf("donner taille memoire de l'ordinateur \n");
-  scanf("%d",&stockOrd[*PnbOrd].tailleMemoir);
-  printf("donner taille disque de l'ordinateur \n");
-  scanf("%d",&stockOrd[*PnbOrd].tailleDisque);
-  printf("donner marque de l'ordinateur \n");
-  scanf("%s",&stockOrd[*PnbOrd].marque);
-  printf("donner systeme d'exploitation de l'ordinateur \n");
-  scanf("%s",&stockOrd[*PnbOrd].sysExp);
-  *PnbOrd=*PnbOrd+1;
-  }
-}
-void ajouterVoit(voiture stockVoit[], int *PnbVoit){
-  if (*PnbVoit==50)
-  {
-    printf("stockage est plein \n");
-  }
-  
-  else{
-  printf("donner reference de la voiture \n");
-  scanf("%d",&stockVoit[*PnbVoit].ref);
-  printf("donner le type du moteur de la voiture\n");
-  scanf("%s",&stockVoit[*PnbVoit].typeMoteur);
-  printf("donner la vitesse maximale de la voiture \n");
-  scanf("%d",&stockVoit[*PnbVoit].vitesseMax);
-  printf("donner le prix de la voiture \n");
-  scanf("%d",&stockVoit[*PnbVoit].prix);
-  printf("donner la marque de la voiture \n");
-  scanf("%s",&stockVoit[*PnbVoit].marque);
-  *PnbVoit = *PnbVoit + 1;
-  }
-}
-void afficherVoit(voiture stockVoit[],int reff , int *PnbVoit){
-  int test=0;
-  if(*PnbVoit==0){
-    printf("il faut d'abord ajouter des voitures \n");
-  }
-  for(i=0;i<*PnbVoit;i++){
-    if(stockVoit[i].ref==reff){
-      printf("la reference est : %d \n la vitesse maximale est : %d \n le prix est : %d \n la marque est : %s \n le type moteur est : %s \n",stockVoit[i].ref,stockVoit[i].vitesseMax,stockVoit[i].prix,stockVoit[i].marque,stockVoit[i].typeMoteur);
-      test=1;
-      break;
+void ajouterOrd(ordinateur stockOrd[], int *PnbOrd) {
+    if (*PnbOrd == 50) {
+        printf("Stock est plein\n");
+    } else {
+        printf("Donnez la référence de l'ordinateur: ");
+        scanf("%d", &stockOrd[*PnbOrd].ref);
+        printf("Donnez la taille mémoire de l'ordinateur: ");
+        scanf("%d", &stockOrd[*PnbOrd].tailleMemoir);
+        printf("Donnez la taille disque de l'ordinateur: ");
+        scanf("%d", &stockOrd[*PnbOrd].tailleDisque);
+        printf("Donnez la marque de l'ordinateur: ");
+        scanf("%19s", stockOrd[*PnbOrd].marque);
+        printf("Donnez le système d'exploitation de l'ordinateur: ");
+        scanf("%19s", stockOrd[*PnbOrd].sysExp);
+        (*PnbOrd)++;
     }
-  }
-  if(test==0){
-    printf("la reference n'existe pas \n");
-  }
 }
 
-void afficherOrd(ordinateur stockOrd[],int reff,int *PnbOrd ){
-  int test=0;
-  if(*PnbOrd==0){
-    printf("il faut d'abord ajouter des ordinateurs \n");
-  }
-  for(i=0;i<*PnbOrd;i++){
-    if(stockOrd[i].ref==reff){
-      printf("la reference est : %d \n la memoire est : %d \n le taille disque est : %d \n la marque est : %s \n le systeme d'exploitation est : %s \n",stockOrd[i].ref,stockOrd[i].tailleMemoir,stockOrd[i].tailleDisque,stockOrd[i].marque,stockOrd[i].sysExp);
-      test=1;
-      break;
+void ajouterVoit(voiture stockVoit[], int *PnbVoit) {
+    if (*PnbVoit == 50) {
+        printf("Stock est plein\n");
+    } else {
+        printf("Donnez la référence de la voiture: ");
+        scanf("%d", &stockVoit[*PnbVoit].ref);
+        printf("Donnez le type du moteur de la voiture: ");
+        scanf("%19s", stockVoit[*PnbVoit].typeMoteur);
+        printf("Donnez la vitesse maximale de la voiture: ");
+        scanf("%d", &stockVoit[*PnbVoit].vitesseMax);
+        printf("Donnez le prix de la voiture: ");
+        scanf("%d", &stockVoit[*PnbVoit].prix);
+        printf("Donnez la marque de la voiture: ");
+        scanf("%19s", stockVoit[*PnbVoit].marque);
+        (*PnbVoit)++;
     }
-  }
-  if(test==0){
-    printf("la reference n'existe pas \n");
-  }
+}
+
+void afficherVoit(voiture stockVoit[], int reff, int PnbVoit) {
+    int test = 0;
+    if (PnbVoit == 0) {
+        printf("Il faut d'abord ajouter des voitures\n");
+    }
+
+    for (int i = 0; i < PnbVoit; i++) {
+        if (stockVoit[i].ref == reff) {
+            printf("La référence est : %d\nLa vitesse maximale est : %d\nLe prix est : %d\nLa marque est : %s\nLe type moteur est : %s\n",
+                stockVoit[i].ref, stockVoit[i].vitesseMax, stockVoit[i].prix, stockVoit[i].marque, stockVoit[i].typeMoteur);
+            test = 1;
+            break;
+        }
+    }
+
+    if (test == 0) {
+        printf("La référence n'existe pas\n");
+    }
+}
+
+void afficherOrd(ordinateur stockOrd[], int reff, int PnbOrd) {
+    int test = 0;
+    if (PnbOrd == 0) {
+        printf("Il faut d'abord ajouter des ordinateurs\n");
+    }
+
+    for (int i = 0; i < PnbOrd; i++) {
+        if (stockOrd[i].ref == reff) {
+            printf("La référence est : %d\nLa mémoire est : %d\nLe taille disque est : %d\nLa marque est : %s\nLe système d'exploitation est : %s\n",
+                stockOrd[i].ref, stockOrd[i].tailleMemoir, stockOrd[i].tailleDisque, stockOrd[i].marque, stockOrd[i].sysExp);
+            test = 1;
+            break;
+        }
+    }
+
+    if (test == 0) {
+        printf("La référence n'existe pas\n");
+    }
 }
