b8dff2d8c29ff459e817578175a6e1d8ad439183
farouk <ladjimifarouk70@gmail.com>
2023-12-06 19:42:33 +0100

    bro wlhhhh rani guleguttttttt

--- calculatrice.js
+++ calculatrice.js
@@ -1,26 +1,38 @@
-let display = document.getElementById('display');
-let expression = '';
+// DOM
+const touches = [...document.querySelectorAll('.bouton')];
+const listeKeycode = touches.map(touche => touche.dataset.key);
+const ecran = document.querySelector('.ecran');
 
-function appendValue(value) {
-    expression += value;
-    display.value = expression;
-}
+document.addEventListener('keydown', (e) => {
+    const valeur = e.keyCode.toString();
+    calculer(valeur)
 
-function clearDisplay() {
-    expression = '';
-    display.value = '';
-}
+})
 
-function deleteLastCharacter() {
-    expression = expression.slice(0, -1);
-    display.value = expression;
-}
+document.addEventListener('click', (e) => {
+    const valeur = e.target.dataset.key;
+    calculer(valeur)
 
-function calculateResult() {
-    try {
-        expression = eval(expression);
-        display.value = expression;
-    } catch (error) {
-        display.value = 'Error';
+})
+
+const calculer = (valeur) => {
+    if (listeKeycode.includes(valeur)) {
+        switch (valeur) {
+            case '8':
+                ecran.textContent = "";
+                break;
+            case '13':
+                const calcul = eval(ecran.textContent);
+                ecran.textContent = calcul;
+                break;
+            default:
+                const indexKeycode = listeKeycode.indexOf(valeur);
+                const touche = touches[indexKeycode];
+                ecran.textContent += touche.innerHTML;
+        }
     }
 }
+
+window.addEventListener('error', (e) => {
+    alert('Une erreur est survenue dans votre calcul : ' + e.message)
+})
